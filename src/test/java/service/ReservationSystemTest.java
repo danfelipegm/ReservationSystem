eee06a89f6712c12474a59d4b083c264d9cb5494
Facultad de Ing Sistemas e informatica <informatica@bga.upb>
2023-10-30 13:41:37 -0500

    test ReservationSystem

--- src/test/java/service/ReservationSystemTest.java
+++ src/test/java/service/ReservationSystemTest.java
@@ -1,28 +1,52 @@
 package service;
 
+import dao.ConferenceRoomDao;
 import model.ConferenceRoom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ReservationSystemTest {
 
     private ReservationSystem system;
+    private ConferenceRoomDao conferenceRoomMock;
 
     @BeforeEach
     public void setUp() {
-        //system = new ReservationSystem();
+        conferenceRoomMock = mock(ConferenceRoomDao.class);
+        system = new ReservationSystem(conferenceRoomMock);
     }
 
     @Test
-    public void testReserveRoom() {
-        // Implementar pruebas aquí
+    public void testReserveRoomWhenIsFalse() {
+        when(conferenceRoomMock.getRoomById("A1")).thenReturn(null);
+        assertEquals(false, system.reserveRoom("A1"));
+        
+    }
+    
+    @Test
+    public void testReserveRoomWhenIsTrue() {
+        ConferenceRoom conferenceRoomBeta = new ConferenceRoom("A1", 5);
+        when(conferenceRoomMock.getRoomById("A1")).thenReturn(conferenceRoomBeta);
+        assertEquals(true, system.reserveRoom("A1"));
+        
     }
 
     @Test
-    public void testCancelReservation() {
-        // Implementar pruebas aquí
+    public void testCancelReservationWhenIsFalse() {
+        when(conferenceRoomMock.getRoomById("A2")).thenReturn(null);
+        assertEquals(false,system.cancelReservation("A2"));
+    }
+    
+    @Test
+    public void testCancelReservationWhenIsTrue() {
+        ConferenceRoom conferenceRoomAlpha = new ConferenceRoom("A2", 5);
+        conferenceRoomAlpha.setReserved(true);
+        when(conferenceRoomMock.getRoomById("A2")).thenReturn(conferenceRoomAlpha);
+        assertEquals(true,system.cancelReservation("A2"));
     }
 
     // Puedes añadir más pruebas según lo consideres necesario
